ef219f20de5a402b51c1b1fb7698510868797427
shaphat-codes <jehoshaphattatiglo99@gmail.com>
2024-04-08 04:36:10 -0700

    successfully connected to railay database

--- database/database.go
+++ database/database.go
@@ -1,23 +1,23 @@
-package database 
+package database
 
 import (
 	"fmt"
-	"gorm.io/gorm"
+
 	"gorm.io/driver/postgres"
-	"os"
+	"gorm.io/gorm"
 )
 
 var Database *gorm.DB
 
 func Connect() {
 	var err error
-	host := os.Getenv("DB_HOST")
-	username := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	databaseName := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
+	// host := os.Getenv("DB_HOST")
+	// username := os.Getenv("DB_USER")
+	// password := os.Getenv("DB_PASSWORD")
+	// databaseName := os.Getenv("DB_NAME")
+	// port := os.Getenv("DB_PORT")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos", host, username, password, databaseName, port)
+	dsn := fmt.Sprintf("postgresql://postgres:dWGkIkpFvlwZigvBZBJcHZQWXWJgitNe@monorail.proxy.rlwy.net:50876/railway")
 	Database, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
